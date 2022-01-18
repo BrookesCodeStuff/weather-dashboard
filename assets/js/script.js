332910abc9795d48f2e0f38556810651e34bcc3e
Brooke <brookepaglia@gmail.com>
2022-01-17 22:22:25 -0500

    initial file setup

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+var apiKey = 'c9f1d008018a1c7896a5f42383fc54fe';
